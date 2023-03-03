f645447369d1f215c4f662a8a494089f60b402a6
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-03 11:28:41 +0100

    started in earnest the Coffee Connoisseur

---
+++ components/banner.js
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1 className="banner__title"><span>Coffee</span> <span>Connoisseur</span></h1>
+        <p className="banner__subtitle">Discover your local coffee shops</p>
+        <button className="banner__button">View stores nearby</button>
+    </div>
+  );
+}
+
+export default Banner;
