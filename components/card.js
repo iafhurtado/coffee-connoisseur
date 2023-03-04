2a4e7bc0def4f7c40ffe47076edbad3ffb6b66da
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-04 11:53:05 +0100

    adding CSS to Card component

--- components/card.js
+++ components/card.js
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import styles from './card.module.css';
+import cls from 'classnames';
 
 const Card = ( props ) => {
   return (
     <Link href={props.href}>
-            <h2>{props.name}</h2>
-            <Image src={props.imgUrl} width={260} height={160} alt="props" />
+
+        <div className={cls("glass", styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+              <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image className={styles.Image} src={props.imgUrl} width={260} height={160} alt="props" />
+          </div>
+        </div>
+
     </Link>
   );
 }
