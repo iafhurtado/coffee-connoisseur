a3b8a902fa869857abbfcdcd5be0f2f84137a2a2
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-07 19:32:38 +0100

    fallback

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,43 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-export default function CoffeeStore() {
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  console.log("params", params);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic id
+      }),
+    },
+  };
+}
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+  console.log("router", router);
 
+  console.log("props", props);
   return (
     <div>
-      Coffee Store {router.query.id}
-      <Link href="/"><a>Back to home</a></Link>
+      Coffee Store Page {router.query.id}
+      <Link href="/">
+        <>Back to home</>
+      </Link>
+      <Link href="/coffee-store/dynamic">
+        <>Go to page dynamic</>
+      </Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
-}
+};
+
+export default CoffeeStore;
