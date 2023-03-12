3eaba868e56adcde76d92723428404581af98af5
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-12 12:56:48 +0100

    started with SWR

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,40 +1,54 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN }).base(process.env.AIRTABLE_BASE_ID);
-
-const table = base("coffee-stores");
-
-console.log({ table });
-
-const createCoffeeStore = async (req, res) => {
-    console.log({ req });
+import {
+    table,
+    getMinifiedRecords,
+    findRecordByFilter,
+  } from "../../lib/airtable";
+  
+  const createCoffeeStore = async (req, res) => {
     if (req.method === "POST") {
       //find a record
   
+      const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+  
       try {
-        const findCoffeeStoreRecords = await table
-          .select({
-            filterByFormula: `id="59f784dd28122f14f9d5d63d"`,
-          })
-          .firstPage();
+        if (id) {
+          const records = await findRecordByFilter(id);
   
-      console.log({ findCoffeeStoreRecords });
+          if (records.length !== 0) {
+            res.json(records);
+          } else {
+            //create a record
+            if (name) {
+              const createRecords = await table.create([
+                {
+                  fields: {
+                    id,
+                    name,
+                    address,
+                    neighbourhood,
+                    voting,
+                    imgUrl,
+                  },
+                },
+              ]);
   
-      if (findCoffeeStoreRecords.length !== 0) {
-        const records = findCoffeeStoreRecords.map((record) => {
-          return {
-            ...record.fields,
-          };
-        });
-        res.json(records);
-      } else {
-        //create a record
-        res.json({ message: "create a record" });
-      }
-    } catch (err) {
-      console.error("Error finding store", err);
-      res.status(500);
-      res.json({ message: "Error finding store", err });
+              const records = getMinifiedRecords(createRecords);
+              res.json(records);
+            } else {
+              res.status(400);
+              res.json({ message: "Id or name is missing" });
+            }
+          }
+        } else {
+          res.status(400);
+          res.json({ message: "Id is missing" });
+        }
+      } catch (err) {
+        console.error("Error creating or finding a store", err);
+        res.status(500);
+        res.json({ message: "Error creating or finding a store", err });
       }
     }
   };
   
+  export default createCoffeeStore;
