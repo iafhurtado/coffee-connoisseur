79f547af4d3ac59a26e7605a4ef59aa7dc33b0d4
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-12 11:43:31 +0100

    building airtable api

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,13 +5,36 @@ const table = base("coffee-stores");
 
 console.log({ table });
 
-const createCoffeeStore = (req, res) => {
-  console.log({ req });
-  if (req.method === "POST") {
-    res.json({ message: "Hi there" });
-  } else {
-    res.json({ message: "method is GET" });
-  }
-};
-
-export default createCoffeeStore;
+const createCoffeeStore = async (req, res) => {
+    console.log({ req });
+    if (req.method === "POST") {
+      //find a record
+  
+      try {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id="59f784dd28122f14f9d5d63d"`,
+          })
+          .firstPage();
+  
+      console.log({ findCoffeeStoreRecords });
+  
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        //create a record
+        res.json({ message: "create a record" });
+      }
+    } catch (err) {
+      console.error("Error finding store", err);
+      res.status(500);
+      res.json({ message: "Error finding store", err });
+      }
+    }
+  };
+  
