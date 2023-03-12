f679f3f3b90e52edf7dfed459f2d0b3343928c41
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-12 10:54:16 +0100

    added airtable api

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,17 @@
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN }).base(process.env.AIRTABLE_BASE_ID);
+
+const table = base("coffee-stores");
+
+console.log({ table });
+
+const createCoffeeStore = (req, res) => {
+  console.log({ req });
+  if (req.method === "POST") {
+    res.json({ message: "Hi there" });
+  } else {
+    res.json({ message: "method is GET" });
+  }
+};
+
+export default createCoffeeStore;
--- pages/index.js
+++ pages/index.js
@@ -36,8 +36,11 @@ export default function Home(props) {
     async function setCoffeeStoresByLocation() {
       if (latLong) {
         try {
-          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
-          console.log({ fetchedCoffeeStores });
+          const response = await fetch(
+            `/api/getCoffeeStoreByLocation?latLong=${latLong}&limit=30`
+          );
+
+          const coffeeStores = await response.json();
           //set coffee stores
         } catch (error) {
           //set error
@@ -46,10 +49,11 @@ export default function Home(props) {
           dispatch({
             type: ACTION_TYPES.SET_COFFEE_STORES,
             payload: {
-              coffeeStores: fetchedCoffeeStores,
+              coffeeStores,
             },
           });
-        }
+          setCoffeeStoresError("");
+        } 
       }
     }
     setCoffeeStoresByLocation();
